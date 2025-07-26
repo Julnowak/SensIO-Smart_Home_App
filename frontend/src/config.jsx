bb0f4626a96cc53ba715af88228c85b3ceaf08ca
Julnowak <julnowak@student.agh.edu.pl>
2025-07-26 16:39:53 +0200

    bp

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -4,7 +4,3 @@ export const API_BASE_URL = isEmulator
     ? "http://10.0.2.2:8000/api/"
     : "http://127.0.0.1:8000/api/";
 
-
-export const WEBSITE_BASE_URL = isEmulator
-    ? "http://10.0.2.2:3000"
-    : "http://127.0.0.1:3000";
